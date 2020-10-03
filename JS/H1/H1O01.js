d686c5efce0c3af2c84dd00bc45df56297738298
Thomas05t <151476@csgaugustinus.nl>
2020-10-03 17:00:25 +0000

    mis

--- JS/H1/H1O01.js
+++ JS/H1/H1O01.js
@@ -1,24 +1,45 @@
 var startscreen;
 var song;
-var slider; 
+var slider;
+var isInGame = false
+var gamescreen;
 
 laadJavascriptFile('JS/P5/addons/p5.sound.js');
 
 function preload() {
-song = loadSound('sounds/background/diekaplyn.mp3');
-startscreen = loadImage("images/backgrounds/ZuidAfrika1png.png");
+    song = loadSound('sounds/background/diekaplyn.mp3');
+    startscreen = loadImage("images/backgrounds/ZuidAfrika1png.png");
+    gamescreen= loadImage("images/backgrounds/ddd.png");
 }
 
 function setup() {
-canvas = createCanvas(1920,1080);
-canvas.parent('processing');
-song.play();
-slider = createSlider(0, 1, 0.5, 0.1);
-slider.position(800, 400);
-slider.style('width', '160px');
+    canvas = createCanvas(1920, 1080);
+    canvas.parent('processing');
+    song.play();
+    slider = createSlider(0, 1, 0.5, 0.1);
+    slider.position(800, 400);
+    slider.style('width', '160px');
+    textFont("Verdana");
+    textSize(30);
+    noStroke();
 }
 
 function draw() {
-background(startscreen); 
-song.setVolume(slider.value()); 
+
+    song.setVolume(slider.value());
+
+
+    if (isInGame) {
+        background(gamescreen)
+        
+        
+    } else {
+        background(startscreen);
+        fill('white');
+        text("Klik op die muisknoppie om te begin")
+        if (mouseIsPressed) {
+            isInGame = true
+        }
+    }
 }
+
