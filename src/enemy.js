62a0e47beef12ae11bb756160009cf5dba316188
rijkvp <rijkvanputten@hotmail.nl>
2020-11-03 17:12:05 +0100

    De CHEAT is weg :(

--- src/enemy.js
+++ src/enemy.js
@@ -1,8 +1,8 @@
 class Enemy {
     constructor(plaatje) {
         this.sprite = plaatje;
-        this.width = this.sprite.width / 3;
-        this.height = this.sprite.height / 3;
+        this.width = this.sprite.width / 4;
+        this.height = this.sprite.height / 4;
         this.x = random(0, width - this.width);
         this.y = random(0, height - this.height);
         this.isDood = false;
@@ -11,6 +11,9 @@ class Enemy {
 
     update() {
         this.lifeTime -= (deltaTime / 1000);
+        if (this.lifeTime < 0) {
+            this.isDood = true;
+        }
     }
 
     teken() {
@@ -18,14 +21,8 @@ class Enemy {
     }
 
     valMisschienDood(x, y) {
-        if (this.lifeTime < 0) {
-            this.isDood = true;
-            return;
-        }
         if (x > this.x && x < this.x + this.width && y > this.y && y < this.y + this.height) {
             this.isDood = true;
-            return
         }
-        this.isDood = false;
     }
 }
